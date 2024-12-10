64892dd1d268bf4f5b1442093a8b36ce289c046e
AmmarEmad-2004 <za0662003@email.com>
2024-12-10 20:26:48 +0200

    create custom buttom

--- lib/views/widgets/add_note_buttom_sheet.dart
+++ lib/views/widgets/add_note_buttom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_buttom.dart';
 import 'package:notes_app/views/widgets/custom_text_feild.dart';
 
 class AddNoteButtomSheet extends StatelessWidget {
@@ -8,22 +9,33 @@ class AddNoteButtomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 8.0),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextFeild(
-            hint: 'Enter Title',
-          ),
-          SizedBox(
-            height: 12,
-          ),
-          CustomTextFeild(
-            hint: 'Contant',
-            maxlines: 5,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextFeild(
+              hint: 'Enter Title',
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            CustomTextFeild(
+              hint: 'Contant',
+              maxlines: 8,
+            ),
+            SizedBox(
+              height: 64,
+            ),
+            CustomButtom(
+              text: 'Add',
+            ),
+            SizedBox(
+              height: 32,
+            ),
+          ],
+        ),
       ),
     );
   }
